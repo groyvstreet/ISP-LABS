9d0c114ff9d68242696997a26f6f30efb581b560
groyvstreet <75688738+groyvstreet@users.noreply.github.com>
2021-04-06 10:26:55 +0300

    Lab4

---
+++ Lab4/Lab4_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lab4_1
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct SystemInfo
+    {
+        public ushort wProcessorArchitecture;
+        public ushort wReserved;
+        public uint dwPageSize;
+        public IntPtr lpMinimumApplicationAddress;
+        public IntPtr lpMaximumApplicationAddress;
+        public IntPtr dwActiveProcessorMask;
+        public uint dwNumberOfProcessors;
+        public uint dwProcessorType;
+        public uint dwAllocationGranularity;
+        public ushort wProcessorLevel;
+        public ushort wProcessorRevision;
+    }
+    enum ProcessorArchitecture
+    {
+        x86 = 0,
+        ARM = 5,
+        IA64 = 6,
+        x64 = 9,
+        ARM64 = 12,
+        Unknown = 65535
+    }
+    enum ProcessorType
+    {
+        Intel386 = 386,
+        Intel486 = 486,
+        IntelPentium = 586,
+        IntelIA64 = 2200,
+        AMD = 8664
+    }
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int GetComputerName(StringBuilder buffer, ref int sizeBuffer);
+        [DllImport("kernel32.dll")]
+        static extern void GetNativeSystemInfo(out SystemInfo Info);
+        [DllImport("kernel32.dll")]
+        static extern bool GetPhysicallyInstalledSystemMemory(out long memory);
+
+        static void Main()
+        {
+            StringBuilder computerName = new StringBuilder(20);
+            int length = computerName.Capacity;
+            GetComputerName(computerName, ref length);
+            Console.WriteLine($"Information about {computerName}: \n");
+            GetNativeSystemInfo(out SystemInfo info);
+            Console.WriteLine($"Processor architecture: {(ProcessorArchitecture)info.wProcessorArchitecture}");
+            Console.WriteLine($"Reserved: {info.wReserved}");
+            Console.WriteLine($"Page size: {info.dwPageSize}");
+            Console.WriteLine($"Minimum application address: {info.lpMinimumApplicationAddress}");
+            Console.WriteLine($"Maximum application address: {info.lpMaximumApplicationAddress}");
+            Console.WriteLine($"Active processor mask: {info.dwActiveProcessorMask}");
+            Console.WriteLine($"Number of processors: {info.dwNumberOfProcessors}");
+            Console.WriteLine($"Processor type: {(ProcessorType)info.dwProcessorType}");
+            Console.WriteLine($"Allocation granularity: {info.dwAllocationGranularity}");
+            Console.WriteLine($"Processor level: {info.wProcessorLevel}");
+            Console.WriteLine($"Processor revision: {info.wProcessorRevision}");
+            GetPhysicallyInstalledSystemMemory(out long memory);
+            Console.WriteLine($"RAM: {memory / 1024 / 1024} GB");
+        }
+    }
+}
---
+++ Lab4/Lab4_2/Lab4_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        [DllImport("dllmath.dll")]
+        public static extern int Module(int num);
+        [DllImport("dllmath.dll")]
+        public static extern double ModuleD(double num);
+        [DllImport("dllmath.dll")]
+        public static extern int Max(int num1, int num2);
+        [DllImport("dllmath.dll")]
+        public static extern int Min(int num1, int num2);
+        [DllImport("dllmath.dll")]
+        public static extern int Pow(int num, int level);
+        [DllImport("dllmath.dll")]
+        public static extern double Sin(double x);
+        [DllImport("dllmath.dll")]
+        public static extern double Cos(double x);
+        [DllImport("dllmath.dll")]
+        public static extern double Tg(double x);
+        [DllImport("dllmath.dll")]
+        public static extern double Ctg(double x);
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Module (-5): {Module(-5)}");
+            Console.WriteLine($"Module (-5.5): {ModuleD(-5.5)}");
+            Console.WriteLine($"Max (12, 1): {Max(12, 1)}");
+            Console.WriteLine($"Min (12, 1): {Min(12, 1)}");
+            Console.WriteLine($"2 ^ 3 = {Pow(2, 3)}");
+            Console.WriteLine($"Sin(0.5) = {Sin(0.5)}");
+            Console.WriteLine($"Cos(0.5) = {Cos(0.5)}");
+            Console.WriteLine($"Tg(0.5) = {Tg(0.5)}");
+            Console.WriteLine($"Ctg(0.5) = {Ctg(0.5)}");
+        }
+    }
+}
