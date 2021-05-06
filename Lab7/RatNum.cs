93c979ab21252d20a522bb5ed46237fe7551971a
groyvstreet <75688738+groyvstreet@users.noreply.github.com>
2021-05-06 13:56:03 +0300

    Lab7

--- Lab7/RatNum.cs
+++ Lab7/RatNum.cs
@@ -10,11 +10,13 @@ namespace Lab7
     {
         public int Numerator { set; get; }
         public int Denominator { set; get; }
+
         public RationalNumber(int numerator, int denominator)
         {
             Numerator = numerator;
             Denominator = denominator;
         }
+
         private static int GCD(int a, int b)
         {
             int temp;
@@ -39,24 +41,29 @@ namespace Lab7
 
             return a;
         }
+
         private static int LCM(int a, int b)
         {
             return a * b / GCD(a, b);
         }
+
         public static RationalNumber operator +(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return new RationalNumber(n1.Numerator * lcm / n1.Denominator + n2.Numerator * lcm / n2.Denominator, lcm);
         }
+
         public static RationalNumber operator -(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return new RationalNumber(n1.Numerator * lcm / n1.Denominator - n2.Numerator * lcm / n2.Denominator, lcm);
         }
+
         public static RationalNumber operator *(RationalNumber n1, RationalNumber n2)
         {
             return new RationalNumber(n1.Numerator * n2.Numerator, n1.Denominator * n2.Denominator);
         }
+
         public static RationalNumber operator /(RationalNumber n1, RationalNumber n2)
         {
             if ((n1.Numerator > 0 && n2.Numerator > 0) || (n1.Numerator < 0 && n2.Numerator < 0))
@@ -68,30 +75,36 @@ namespace Lab7
                 return new RationalNumber(-Math.Abs(n1.Numerator * n2.Denominator), Math.Abs(n1.Denominator * n2.Numerator));
             }
         }
+
         public static bool operator >(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return n1.Numerator * lcm / n1.Denominator > n2.Numerator * lcm / n2.Denominator;
         }
+
         public static bool operator <(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return n1.Numerator * lcm / n1.Denominator < n2.Numerator * lcm / n2.Denominator;
         }
+
         public static bool operator ==(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return n1.Numerator * lcm / n1.Denominator == n2.Numerator * lcm / n2.Denominator;
         }
+
         public static bool operator !=(RationalNumber n1, RationalNumber n2)
         {
             int lcm = LCM(n1.Denominator, n2.Denominator);
             return n1.Numerator * lcm / n1.Denominator != n2.Numerator * lcm / n2.Denominator;
         }
+
         public override string ToString()
         {
             return Numerator.ToString() + "/" + Denominator.ToString();
         }
+
         public string ToString(string format)
         {
             if(format == ".")
@@ -101,6 +114,7 @@ namespace Lab7
 
             return "";
         }
+
         public static RationalNumber Parse(string str)
         {
             if(str.Contains('/'))
@@ -118,32 +132,39 @@ namespace Lab7
                 return new RationalNumber(0, 1);
             }
         }
-        public static explicit operator short(RationalNumber n)
+
+        public static implicit operator short(RationalNumber n)
         {
             return (short)(n.Numerator / n.Denominator);
         }
-        public static explicit operator int(RationalNumber n)
+
+        public static implicit operator int(RationalNumber n)
         {
             return n.Numerator / n.Denominator;
         }
-        public static explicit operator long(RationalNumber n)
+
+        public static implicit operator long(RationalNumber n)
         {
             return n.Numerator / n.Denominator;
         }
-        public static explicit operator float(RationalNumber n)
+
+        public static implicit operator float(RationalNumber n)
         {
             return (float)n.Numerator / n.Denominator;
         }
-        public static explicit operator double(RationalNumber n)
+
+        public static implicit operator double(RationalNumber n)
         {
             return (double)n.Numerator / n.Denominator;
         }
+
         public void Reduce()
         {
             int gcd = GCD(Math.Abs(Numerator), Denominator);
             Numerator /= gcd;
             Denominator /= gcd;
         }
+
         public int CompareTo(RationalNumber other)
         {
             return this == other ? 0 : (this > other ? 1 : -1);
